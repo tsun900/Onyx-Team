f161dcad06345e55ec931c3265661b5c3a839f9e
Park, Ewan <ypark0502@gmail.com>
2021-10-12 14:15:57 -0400

    changes poke pic and adds stats

--- pokedex.js
+++ pokedex.js
@@ -3,11 +3,19 @@ const img = get('poke-img')
 const poke_name = get('poke-name')
 const type_1 = get('poke-type-1')
 const type_2 = get('poke-type-2')
-const get_stats = get('stats')
+const get_stats = get('stats-button')
 const go_right = get('right-btn')
 const go_left = get('left-btn')
-
-let stats = {}
+const info_screen = get('poke-screen')
+const hp = get('hp')
+const attack = get('attack')
+const defense = get('defense')
+const special_attack = get('special-attack')
+const special_defense = get('special-defense')
+const speed = get('speed')
+const weight = get('weight')
+const height = get('height')
+let stats = []
 const typeColors = {
   fire: '#FDDFDF',
 	grass: '#DEFDE0',
@@ -29,12 +37,20 @@ const getPokemon = () => {
   fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon_num}`)
     .then(r => r.json())
     .then(poke => {
-      img.src = poke.sprites.front_default
+      img.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon_num}.png`
       console.log(poke.types)
-      poke_name.innerHTML = poke.forms[0].name
-      type_1.innerHTML = poke.types[0].type.name
-      type_2.innerHTML = poke.types[1]?.type.name || 'none'
-      stats = poke.stats
+      poke_name.innerHTML = capitalize(poke.forms[0].name)
+      type_1.innerHTML = capitalize(poke.types[0].type.name)
+      type_2.innerHTML = capitalize(poke.types[1]?.type.name || 'none')
+      stats = poke.stats 
+      weight.textContent = "Weight: " + poke.weight
+      height.textContent = "Height: "+ poke.height
+      hp.textContent = "HP: " + stats[0].base_stat
+      attack.textContent = "Attack: " + stats[1].base_stat
+      defense.textContent = "Defense: " + stats[2].base_stat
+      special_attack.textContent = "Special Attack: " + stats[3].base_stat
+      special_defense.textContent = "Special Defense: " + stats[4].base_stat
+      speed.textContent = "Speed: " + stats[5].base_stat
     })
 }
 
@@ -50,6 +66,7 @@ const prevPoke = () => {
 }
 
 const clearSelected = () => [get_stats].forEach(e => e.classList.remove('selected'))
+const capitalize = (str) => str[0].toUpperCase() + str.substr(1);
 
 document.addEventListener('click', (e) => {
   if (e.target === go_right) {
@@ -57,7 +74,6 @@ document.addEventListener('click', (e) => {
   } else if (e.target === go_left) {
     prevPoke()
   } else if (e.target === get_stats) {
-    //set stats
     clearSelected()
   }
 })
